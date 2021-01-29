250b18ee46b455c5cea2e54142c67f65c5b85f8b
rizky <rizkynear1@gmail.com>
2021-01-29 11:36:19 +0800

    add search and improve some page

--- resources/views/user/layout/master.blade.php
+++ resources/views/user/layout/master.blade.php
@@ -100,7 +100,7 @@
         <div class="h-100 d-flex align-items-center justify-content-center">
             <div class="search-close-switch"><i class="icon_close"></i></div>
             <form class="search-model-form" action="{{ route('user.search') }}">
-                <input type="text" id="search-input" name="keyword" placeholder="Cari judul anime">
+                <input type="text" id="search-input" name="keyword" placeholder="Cari judul anime" value="{{ request()->get('keyword') }}">
             </form>
         </div>
     </div>
