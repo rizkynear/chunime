f7cf9bbd856b2beb6841ef7374d451db35592465
rizky <rizkynear1@gmail.com>
2021-01-29 10:46:14 +0800

    enable webp

--- app/Http/Requests/Admin/AnimeImageRequest.php
+++ app/Http/Requests/Admin/AnimeImageRequest.php
@@ -24,7 +24,7 @@ class AnimeImageRequest extends FormRequest
     public function rules()
     {
         return [
-            'image' => 'required|mimes:jpg,jpeg,png|dimensions:max_width=2500,max_height=2500|max:2000'
+            'image' => 'required|mimes:jpg,jpeg,png,webp|dimensions:max_width=2500,max_height=2500|max:2000'
         ];
     }
 }
